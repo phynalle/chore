a0d34521d9aa1586797047b1bf752c198f3f03b7
phynalle <phynalism@gmail.com>
2018-02-25 01:26:40 +0900

    Modify error::Error
    error::Error (not std::error::Error) is a general purpose error struct in chore.
    Json and DB are removed from error::Error because all errors from serde_json are handled by task::TaskError and errors from rocksdb are handled through error::Error::Message.

--- src/error.rs
+++ src/error.rs
@@ -2,8 +2,6 @@ use std::fmt;
 use std::result;
 use std::io;
 use std::error;
-use serde_json;
-use serde_json::error::Category;
 use rocksdb;
 use task::TaskError;
 
@@ -12,8 +10,6 @@ pub type Result<T> = result::Result<T, Error>;
 #[derive(Debug)]
 pub enum Error {
     Io(io::Error),
-    DB(rocksdb::Error),
-    Json(serde_json::Error),
     Task(TaskError),
     Message(String),
 }
@@ -28,8 +24,6 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Error::Io(ref e) => write!(f, "IO Error: {}", e),
-            Error::DB(ref e) => write!(f, "DB Error: {}", e),
-            Error::Json(ref e) => write!(f, "Json Error: {}", e),
             Error::Task(ref e) => write!(f, "Task: {}", e),
             Error::Message(ref e) => write!(f, "Error: {}", e),
         }
@@ -40,8 +34,6 @@ impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::Io(ref e) => e.description(),
-            Error::DB(ref e) => e.description(),
-            Error::Json(ref e) => e.description(),
             Error::Task(ref e) => e.description(),
             Error::Message(ref e) => e.as_str(),
         }
@@ -50,8 +42,6 @@ impl error::Error for Error {
     fn cause(&self) -> Option<&error::Error> {
         match *self {
             Error::Io(ref e) => Some(e),
-            Error::DB(ref e) => Some(e),
-            Error::Json(ref e) => Some(e),
             Error::Task(ref e) => Some(e),
             _ => None,
         }
@@ -64,23 +54,14 @@ impl From<io::Error> for Error {
     }
 }
 
-impl From<rocksdb::Error> for Error {
-    fn from(err: rocksdb::Error) -> Error {
-        Error::DB(err)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Error {
-        match err.classify() {
-            Category::Io => Error::Io(err.into()),
-            _ => Error::Json(err),
-        }
-    }
-}
-
 impl From<TaskError> for Error {
     fn from(err: TaskError) -> Error {
         Error::Task(err)
+    }
+}
+
+impl From<rocksdb::Error> for Error {
+    fn from(err: rocksdb::Error) -> Error {
+        Error::new(&err)
     }
 }
